387409bca8850043a8a0a5253436fcc965f9e0e6
Richard Focke Fechner <richardfocke@posteo.net>
2023-08-19 12:54:54 +0200

    added new tests for nonconclusions

--- tests/data/test_suite/__init__.py
+++ tests/data/test_suite/__init__.py
@@ -5,6 +5,19 @@ from . import PRD
 _PRD = importlib.resources.files(PRD)
 
 PositiveEntailmentTest = namedtuple("PositiveEntailmentTest", ("premise", "conclusion"))
+NegativeEntailmentTest = namedtuple("PositiveEntailmentTest", ("premise", "conclusion"))
+class NegativeEntailmentTest:
+    def __init__(self, premise, nonconclusion, import001=None):
+        self.premise = str(premise)
+        self.nonconclusion = str(nonconclusion)
+        self.import001 = str(import001) if import001 is not None else None
+
+    def __iter__(self):
+        yield self.premise
+        yield self.nonconclusion
+        if self.import001 is not None:
+            yield self.import001
+
 PET_Assert = PositiveEntailmentTest(
         _PRD.joinpath("PositiveEntailmentTest/Assert/Assert-premise.rif"),
         _PRD.joinpath("PositiveEntailmentTest/Assert/Assert-conclusion.rif"),
@@ -24,6 +37,16 @@ PET_Modify = PositiveEntailmentTest(
 PET_Modify_loop = PositiveEntailmentTest(
         _PRD.joinpath("PositiveEntailmentTest/Modify_loop/Modify_loop-premise.rif"),
         _PRD.joinpath("PositiveEntailmentTest/Modify_loop/Modify_loop-conclusion.rif"),
+        )
+
+NET_Retract = NegativeEntailmentTest(
+        _PRD.joinpath("NegativeEntailmentTest/Retract/Retract-premise.rif"),
+        _PRD.joinpath("NegativeEntailmentTest/Retract/Retract-nonconclusion.rif"),
+        )
+NET_RDF_Combination_SubClass_5 = NegativeEntailmentTest(
+        _PRD.joinpath("NegativeEntailmentTest/RDF_Combination_SubClass_5/RDF_Combination_SubClass_5-premise.rif"),
+        _PRD.joinpath("NegativeEntailmentTest/RDF_Combination_SubClass_5/RDF_Combination_SubClass_5-nonconclusion.rif"),
+        _PRD.joinpath("NegativeEntailmentTest/RDF_Combination_SubClass_5/RDF_Combination_SubClass_5-import001.rif"),
         )
 
 PositiveEntailmentTests = [PET_Assert,
--- tests/test_rif_basic.py
+++ tests/test_rif_basic.py
@@ -56,3 +56,31 @@ def test_PositiveEntailmentTests(testinfo):
             "facts:\n%s" % (list(q.machine.get_facts()), rif_facts))
     assert rif_facts, "couldnt load conclusion rif_facts directly"
     assert q.check(rif_facts), "Missing expected conclusions"
+
+@pytest.mark.parametrize("testinfo",[
+    pytest.param(data.test_suite.NET_Retract),
+    pytest.param(data.test_suite.NET_RDF_Combination_SubClass_5, marks=pytest.mark.skip("implications are not supported yet")),
+    ])
+def test_NegativeEntailmentTests(testinfo):
+    testinfo.premise
+    testinfo.nonconclusion
+    if testinfo.import001 is not None:
+        q3 = testinfo.import001
+        raise NotImplementedError(repr(testinfo.import001))
+    try:
+        g = rdflib.Graph().parse(testinfo.premise, format="rif")
+        nonconc_graph = rdflib.Graph().parse(testinfo.nonconclusion, format="rif")
+    except rdflib.plugin.PluginException:
+        pytest.skip("Need rdflib parser plugin to load RIF-file")
+    logger.info("premise in ttl:\n%s" % g.serialize())
+
+    q = fockes_reasoner.simpleLogicMachine.from_rdf(g)
+    myfacts = q.run()
+    logger.info("Not expected conclusions in ttl:\n%s" % nonconc_graph.serialize())
+    rif_facts = [f for f in rdfmodel().import_graph(conc_graph) if not isinstance(f, rdflib.term.Node)]
+    logger.info("All facts after machine has run:\n%s\n\nexpected "
+            "facts:\n%s" % (list(q.machine.get_facts()), rif_facts))
+    assert rif_facts, "couldnt load conclusion rif_facts directly"
+    assert not q.check(rif_facts), "Not expected conclusions"
+
+
