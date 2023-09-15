2301b5c9334bc8a8a0dfbcd595ceb142284c013d
WhiteGobo <richardfechner@posteo.net>
2023-09-15 20:51:28 +0200

    added BLD testsuite

--- tests/data/test_suite/__init__.py
+++ tests/data/test_suite/__init__.py
@@ -6,6 +6,7 @@ from . import PRD
 _PRD = importlib.resources.files(PRD)
 from . import Core
 _Core = importlib.resources.files(Core)
+from . import BLD
 
 #PositiveEntailmentTest = namedtuple("PositiveEntailmentTest", ("premise", "conclusion"))
 class PositiveEntailmentTest:
@@ -291,3 +292,181 @@ NET_RDF_Combination_SubClass = NegativeEntailmentTest(
         {"http://www.w3.org/2005/rules/test/repository/tc/RDF_Combination_SubClass/RDF_Combination_SubClass-import001":
         tmp.joinpath("RDF_Combination_SubClass-import001.ttl")},
         )
+
+
+from .BLD.PositiveEntailmentTest import\
+        Arbitrary_Entailment,\
+        Chaining_strategy_numericadd_2,\
+        Chaining_strategy_numericsubtract_1,\
+        Class_Membership,\
+        Classificationinheritance,\
+        ElementEqualityFromListEquality,\
+        EntailEverything,\
+        Equality_in_conclusion_1,\
+        Equality_in_conclusion_2,\
+        Equality_in_conclusion_3,\
+        Equality_in_condition,\
+        Factorial_Functional,\
+        Factorial_Relational,\
+        IRI_from_IRI,\
+        Inconsistent_Entailment,\
+        IndividualData_Separation_Inconsistency,\
+        ListConstantEquality,\
+        ListEqualityFromElementEquality,\
+        ListLiteralEquality,\
+        Multiple_IRIs_from_String,\
+        Multiple_Strings_from_IRI,\
+        Named_Arguments,\
+        RDF_Combination_Member_1,\
+        RDF_Combination_SubClass_4,\
+        RDF_Combination_SubClass_6,\
+        YoungParentDiscount_1
+
+tmp = files(Arbitrary_Entailment)
+PET_Arbitrary_Entailment = PositiveEntailmentTest(
+        tmp.joinpath("Arbitrary_Entailment-premise.rif"),
+        tmp.joinpath("Arbitrary_Entailment-conclusion.rif"),
+        )
+
+tmp = files(Chaining_strategy_numericadd_2)
+PET_Chaining_strategy_numericadd_2 = PositiveEntailmentTest(
+        tmp.joinpath("Chaining_strategy_numeric-add_2-premise.rif"),
+        tmp.joinpath("Chaining_strategy_numeric-add_2-conclusion.rif"),
+        )
+tmp = files(Chaining_strategy_numericsubtract_1)
+PET_Chaining_strategy_numericsubtract_1 = PositiveEntailmentTest(
+        tmp.joinpath("Chaining_strategy_numeric-subtract_1-premise.rif"),
+        tmp.joinpath("Chaining_strategy_numeric-subtract_1-conclusion.rif"),
+        )
+tmp = files(Class_Membership)
+PET_Class_Membership = PositiveEntailmentTest(
+        tmp.joinpath("Class_Membership-premise.rif"),
+        tmp.joinpath("Class_Membership-conclusion.rif"),
+        )
+tmp = files(Classificationinheritance)
+PET_Classificationinheritance = PositiveEntailmentTest(
+        tmp.joinpath("Classificationinheritance-premise.rif"),
+        tmp.joinpath("Classificationinheritance-conclusion.rif"),
+        )
+tmp = files(ElementEqualityFromListEquality)
+PET_ElementEqualityFromListEquality = PositiveEntailmentTest(
+        tmp.joinpath("ElementEqualityFromListEquality-premise.rif"),
+        tmp.joinpath("ElementEqualityFromListEquality-conclusion.rif"),
+        )
+tmp = files(EntailEverything)
+PET_EntailEverything = PositiveEntailmentTest(
+        tmp.joinpath("EntailEverything-premise.rif"),
+        tmp.joinpath("EntailEverything-conclusion.rif"),
+        )
+tmp = files(Equality_in_conclusion_1)
+PET_Equality_in_conclusion_1 = PositiveEntailmentTest(
+        tmp.joinpath("Equality_in_conclusion_1-premise.rif"),
+        tmp.joinpath("Equality_in_conclusion_1-conclusion.rif"),
+        )
+tmp = files(Equality_in_conclusion_2)
+PET_Equality_in_conclusion_2 = PositiveEntailmentTest(
+        tmp.joinpath("Equality_in_conclusion_2-premise.rif"),
+        tmp.joinpath("Equality_in_conclusion_2-conclusion.rif"),
+        )
+tmp = files(Equality_in_conclusion_3)
+PET_Equality_in_conclusion_3 = PositiveEntailmentTest(
+        tmp.joinpath("Equality_in_conclusion_3-premise.rif"),
+        tmp.joinpath("Equality_in_conclusion_3-conclusion.rif"),
+        )
+tmp = files(Equality_in_condition)
+PET_Equality_in_condition = PositiveEntailmentTest(
+        tmp.joinpath("Equality_in_condition-premise.rif"),
+        tmp.joinpath("Equality_in_condition-conclusion.rif"),
+        )
+tmp = files(Factorial_Functional)
+PET_Factorial_Functional = PositiveEntailmentTest(
+        tmp.joinpath("Factorial_Functional-premise.rif"),
+        tmp.joinpath("Factorial_Functional-conclusion.rif"),
+        )
+tmp = files(Factorial_Relational)
+PET_Factorial_Relational = PositiveEntailmentTest(
+        tmp.joinpath("Factorial_Relational-premise.rif"),
+        tmp.joinpath("Factorial_Relational-conclusion.rif"),
+        )
+tmp = files(IRI_from_IRI)
+PET_IRI_from_IRI = PositiveEntailmentTest(
+        tmp.joinpath("IRI_from_IRI-premise.rif"),
+        tmp.joinpath("IRI_from_IRI-conclusion.rif"),
+        )
+tmp = files(Inconsistent_Entailment)
+PET_Inconsistent_Entailment = PositiveEntailmentTest(
+        tmp.joinpath("Inconsistent_Entailment-premise.rif"),
+        tmp.joinpath("Inconsistent_Entailment-conclusion.rif"),
+        )
+tmp = files(IndividualData_Separation_Inconsistency)
+PET_IndividualData_Separation_Inconsistency = PositiveEntailmentTest(\
+        tmp.joinpath("IndividualData_Separation_Inconsistency-premise.rif"),
+        tmp.joinpath("IndividualData_Separation_Inconsistency-conclusion.rif"),
+        )
+tmp = files(ListConstantEquality)
+PET_ListConstantEquality = PositiveEntailmentTest(
+        tmp.joinpath("ListConstantEquality-premise.rif"),
+        tmp.joinpath("ListConstantEquality-conclusion.rif"),
+        )
+tmp = files(ListEqualityFromElementEquality)
+PET_ListEqualityFromElementEquality = PositiveEntailmentTest(
+        tmp.joinpath("ListEqualityFromElementEquality-premise.rif"),
+        tmp.joinpath("ListEqualityFromElementEquality-conclusion.rif"),
+        )
+tmp = files(ListLiteralEquality)
+PET_ListLiteralEquality = PositiveEntailmentTest(
+        tmp.joinpath("ListLiteralEquality-premise.rif"),
+        tmp.joinpath("ListLiteralEquality-conclusion.rif"),
+        )
+tmp = files(Multiple_IRIs_from_String)
+PET_Multiple_IRIs_from_String = PositiveEntailmentTest(
+        tmp.joinpath("Multiple_IRIs_from_String-premise.rif"),
+        tmp.joinpath("Multiple_IRIs_from_String-conclusion.rif"),
+        )
+tmp = files(Multiple_Strings_from_IRI)
+PET_Multiple_Strings_from_IRI = PositiveEntailmentTest(
+        tmp.joinpath("Multiple_Strings_from_IRI-premise.rif"),
+        tmp.joinpath("Multiple_Strings_from_IRI-conclusion.rif"),
+        )
+tmp = files(Named_Arguments)
+PET_Named_Arguments = PositiveEntailmentTest(
+        tmp.joinpath("Named_Arguments-premise.rif"),
+        tmp.joinpath("Named_Arguments-conclusion.rif"),
+        )
+tmp = files(RDF_Combination_Member_1)
+PET_RDF_Combination_Member_1 = PositiveEntailmentTest(
+        tmp.joinpath("RDF_Combination_Member_1-premise.rif"),
+        tmp.joinpath("RDF_Combination_Member_1-conclusion.rif"),
+        )
+tmp = files(RDF_Combination_SubClass_4)
+PET_RDF_Combination_SubClass_4 = PositiveEntailmentTest(
+        tmp.joinpath("RDF_Combination_SubClass_4-premise.rif"),
+        tmp.joinpath("RDF_Combination_SubClass_4-conclusion.rif"),
+        )
+tmp = files(RDF_Combination_SubClass_6)
+PET_RDF_Combination_SubClass_6 = PositiveEntailmentTest(
+        tmp.joinpath("RDF_Combination_SubClass_6-premise.rif"),
+        tmp.joinpath("RDF_Combination_SubClass_6-conclusion.rif"),
+        )
+tmp = files(YoungParentDiscount_1)
+PET_YoungParentDiscount_1 = PositiveEntailmentTest(
+        tmp.joinpath("YoungParentDiscount_1-premise.rif"),
+        tmp.joinpath("YoungParentDiscount_1-conclusion.rif"),
+        )
+
+from .BLD.NegativeEntailmentTest import\
+        Classification_noninheritance,\
+        Named_Argument_Uniterms_nonpolymorphic,\
+        OpenLists,\
+        RDF_Combination_SubClass_3,\
+        RDF_Combination_SubClass_5
+
+tmp = files(Classification_noninheritance)
+tmp = files(Named_Argument_Uniterms_nonpolymorphic)
+tmp = files(OpenLists)
+tmp = files(RDF_Combination_SubClass_3)
+tmp = files(RDF_Combination_SubClass_5)
+
+from .BLD.NegativeSyntaxTest import No_free_variables
+
+tmp = files(No_free_variables)
